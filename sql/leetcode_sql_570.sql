7ae7941e7d9df8aefb4e85fdac477df716647bca
ccubc <chengchen1215@gmail.com>
2024-03-27 23:52:25 -0400

    原来张悬的歌已经那么老了，她的吉他真的不难弹。今天甚至教会了我的吉他老师一首张悬的歌，也是醉了。

---
+++ sql/leetcode_sql_570.sql
@@ -0,0 +1,58 @@
+"
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+"
+
+# Write your MySQL query statement below
+with count_direct_reports as (
+    SELECT COUNT(*) as count_dr,
+    managerId
+    from Employee
+    GROUP BY managerId
+)
+SELECT name from count_direct_reports
+RIGHT JOIN Employee
+ON count_direct_reports.managerId = Employee.id
+WHERE count_direct_reports.count_dr > 4
