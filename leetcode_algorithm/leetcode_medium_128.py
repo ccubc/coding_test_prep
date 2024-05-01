81f07f7cedaaa55ca4749dbd771b85f4269ec5b7
ccubc <chengchen1215@gmail.com>
2024-04-30 22:39:54 -0400

    neetcode 9/150 写了一个类似brute force的解法但也是O(N) 可以研究一下更优雅的解法

--- leetcode_algorithm/leetcode_medium_128.py
+++ leetcode_algorithm/leetcode_medium_128.py
@@ -41,3 +41,38 @@ class Solution:
             ans = max(ans, dfs(n))
 
         return ans
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        min_max_dict = {}
+        ans = 0
+        for i in nums:
+            if i in min_max_dict:
+                # seen
+                continue
+            min_max_dict[i] = [i, i]
+
+            if i-1 in min_max_dict:
+                # e.g. cur number is 4, we saw 3 has been seen before
+                # extend consecutive number range
+                min_max_dict[i][0] = min_max_dict[i-1][0]
+                # update min_max_dict[i-1][0]'s right boundary
+                min_max_dict[min_max_dict[i-1][0]][1] = min_max_dict[i][1]
+
+            if i+1 in min_max_dict:
+                # e.g. cur number is 4, we saw 5 has been seen before
+                # extend consecutive number range
+                min_max_dict[i][1] = min_max_dict[i+1][1]
+                # update min_max_dict[i+1][1]'s left boundary
+                min_max_dict[min_max_dict[i+1][1]][0] = min_max_dict[i][0]
+            
+                if i-1 in min_max_dict:
+                    # update the left ends's limits again
+                    min_max_dict[min_max_dict[i-1][0]][1] = min_max_dict[i+1][1]
+        return max([v[1]-v[0] for v in min_max_dict.values()])+1
