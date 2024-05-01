5ae7a35f553e572f4796ccf77d41fcda3134ec2a
ccubc <chengchen1215@gmail.com>
2024-04-30 20:44:45 -0400

    neetcode 3/150

--- leetcode_algorithm/leetcode_easy_1.py
+++ leetcode_algorithm/leetcode_easy_1.py
@@ -95,3 +95,18 @@ class Solution(object):
                 d[n] = i
             else:
                 return [d[target-n], i]
+
+# 20240430
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, n in enumerate(nums):
+            if target-n in d:
+                return [d[target-n], i]
+            else:
+                d[n] = i
