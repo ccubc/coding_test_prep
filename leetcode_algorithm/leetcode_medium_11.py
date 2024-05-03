c15cb921aabe733d31488b31bd389b663b7cb426
ccubc <chengchen1215@gmail.com>
2024-05-02 23:51:07 -0400

    neetcode 13/150

--- leetcode_algorithm/leetcode_medium_11.py
+++ leetcode_algorithm/leetcode_medium_11.py
@@ -92,4 +92,20 @@ class Solution:
             else:
                 r -= 1
         return ans
-                
+
+# 20240502
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+        ans = 0
+        while l < r:
+            ans = max(ans, min(height[l], height[r])*(r-l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return ans    
