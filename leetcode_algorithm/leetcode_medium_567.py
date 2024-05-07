468f60bcc71c88541671a96e3a658f7e2d9cdf30
Cheng Chen <43866277+ccubc@users.noreply.github.com>
2024-05-06 22:59:35 -0400

    neetcode 18/150 不那么暴力的解法（但是丑）

--- leetcode_algorithm/leetcode_medium_567.py
+++ leetcode_algorithm/leetcode_medium_567.py
@@ -30,3 +30,33 @@ class Solution:
             if c1 == c2:
                 return True
         return False
+
+# 不那么暴力的解法
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        c1 = Counter(s1)
+        matches = []
+        c2 = Counter(s2[:len(s1)])
+        for c in "qwertyuiopasdfghjklzxcvbnm":
+            if c1.get(c, 0) == c2.get(c, 0):
+                matches.append(1)
+            else:
+                matches.append(0)
+        cur_matched_ct = sum(matches)
+        if cur_matched_ct == 26:
+            return True
+        for i in range(len(s2) - len(s1)):
+            if cur_matched_ct == 26:
+                return True
+            c2[s2[i]] -= 1
+            if c1.get(s2[i], 0) == c2.get(s2[i], 0):
+                cur_matched_ct += 1
+            elif c1.get(s2[i], 0) == c2.get(s2[i], 0) + 1:
+                cur_matched_ct -= 1
+            c2[s2[i+len(s1)]] += 1
+            if c1.get(s2[i+len(s1)], 0) == c2.get(s2[i+len(s1)], 0):
+                cur_matched_ct += 1
+            elif c1.get(s2[i+len(s1)], 0) == c2.get(s2[i+len(s1)], 0) - 1:
+                cur_matched_ct -= 1
+        return cur_matched_ct == 26
